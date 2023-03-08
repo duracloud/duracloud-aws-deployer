bfa288a08577e5725489b6f9c237624e7f8bbcf9
Mark Cooper <mark.c.cooper@outlook.com>
2023-03-08 10:04:32 -0800

    Facilitate more flexible provider configuration (#5)
    * Add account/env disclaimer, fix markdown
    * Remove project level backend cfg
    * Incorporate provider configuration with backend
    * Don't presuppose use of dynamodb for tf state lock
    * In order to support more flexible backend / provider cfg add
    docs for creating a lock table (equivalent to the state bucket)
    rather than require it with TF.

--- env/sample/mill/backend.tf
+++ env/sample/mill/backend.tf
@@ -1,10 +1,15 @@
 terraform {
- backend "s3" {
-    bucket         =  "<your bucket here>"
+  backend "s3" {
+    bucket         = "<your bucket here>"
     profile        = "<your aws profile here>"
     region         = "<your aws region here>"
     key            = "terraform/duracloud/mill/tf-state"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "<your table here>"
   }
+}
+
+provider "aws" {
+  profile    = "<your aws profile here>"
+  region     = "<your aws region here>"
 }
--- env/sample/shared/dynamo.tf
+++
@@ -1,1 +0,0 @@
-../../../shared/dynamo.tf

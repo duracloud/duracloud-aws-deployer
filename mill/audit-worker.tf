652f0ec7e2c6c3f38350aa8d8a86e829677212b3
Daniel Bernstein <dabylon@gmail.com>
2023-06-09 09:42:43 -0700

    Make audit, bit, and dup worker maximum instance count configurable.

--- mill/audit-worker.tf
+++ mill/audit-worker.tf
@@ -22,7 +22,7 @@ resource "aws_autoscaling_group" "audit_worker_asg" {
   name                 = "${var.stack_name}-audit-worker-asg"
   launch_configuration = aws_launch_configuration.audit_worker_launch_config.name
   vpc_zone_identifier  = [data.aws_subnet.duracloud_a.id, data.aws_subnet.duracloud_c.id, data.aws_subnet.duracloud_d.id]
-  max_size             = 10
+  max_size             = var.audit_worker_max
   min_size             = 1
 }
 

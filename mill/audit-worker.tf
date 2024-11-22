8c7684e7886c4906f16bbf48c9ff64766e1b3bbb
Mark Cooper <mark.c.cooper@outlook.com>
2024-11-22 09:26:33 -0800

    Replaces launch configuration resources with launch template (#54)
    Recommended by AWS. Required to use fuller range of instance types.

--- mill/audit-worker.tf
+++ mill/audit-worker.tf
@@ -1,29 +1,46 @@
 # config the audit worker launch config, autoscaling group, alarms, etc
 
-resource "aws_launch_configuration" "audit_worker_launch_config" {
-  name_prefix          = "${var.stack_name}-audit-worker-launch-config_"
-  image_id             = local.node_image_id
-  instance_type        = var.worker_instance_class
-  iam_instance_profile = data.aws_iam_instance_profile.duracloud.name
-  security_groups      = [aws_security_group.mill_instance.id]
-  key_name             = var.ec2_keypair
-  spot_price           = var.worker_spot_price
-  user_data            = templatefile("${path.module}/resources/cloud-init.tpl", merge(local.cloud_init_props, { node_type = "audit-worker" }))
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 60
+resource "aws_launch_template" "audit_worker_launch_template" {
+  name_prefix            = "${var.stack_name}-audit-worker-launch-template_"
+  image_id               = local.node_image_id
+  instance_type          = var.worker_instance_class
+  vpc_security_group_ids = [aws_security_group.mill_instance.id]
+  key_name               = var.ec2_keypair
+  user_data              = base64encode(templatefile("${path.module}/resources/cloud-init.tpl", merge(local.cloud_init_props, { node_type = "audit-worker" })))
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      volume_size = 60
+      volume_type = "gp2"
+    }
   }
+
+  iam_instance_profile { name = data.aws_iam_instance_profile.duracloud.name }
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price = var.worker_spot_price
+    }
+  }
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_autoscaling_group" "audit_worker_asg" {
-  name                 = "${var.stack_name}-audit-worker-asg"
-  launch_configuration = aws_launch_configuration.audit_worker_launch_config.name
-  vpc_zone_identifier  = [data.aws_subnet.duracloud_a.id, data.aws_subnet.duracloud_c.id, data.aws_subnet.duracloud_d.id]
-  max_size             = var.audit_worker_max
-  min_size             = 1
+  name                = "${var.stack_name}-audit-worker-asg"
+  vpc_zone_identifier = [data.aws_subnet.duracloud_a.id, data.aws_subnet.duracloud_c.id, data.aws_subnet.duracloud_d.id]
+  max_size            = var.audit_worker_max
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.audit_worker_launch_template.id
+    version = aws_launch_template.audit_worker_launch_template.latest_version
+  }
+
   tag {
     key                 = "Name"
     value               = "${var.stack_name}-audit-worker"
--- mill/sentinel.tf
+++ mill/sentinel.tf
@@ -1,27 +1,38 @@
 # config the sentinel
-resource "aws_launch_configuration" "sentinel_launch_config" {
-  name_prefix          = "${var.stack_name}-sentinel-launch-config_"
-  image_id             = local.node_image_id
-  instance_type        = var.sentinel_instance_class
-  iam_instance_profile = data.aws_iam_instance_profile.duracloud.name
-  security_groups      = [aws_security_group.mill_instance.id]
-  key_name             = var.ec2_keypair
-  user_data            = templatefile("${path.module}/resources/cloud-init.tpl", merge(local.cloud_init_props, { node_type = "sentinel" }))
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 20
+resource "aws_launch_template" "sentinel_launch_template" {
+  name_prefix            = "${var.stack_name}-sentinel-launch-template_"
+  image_id               = local.node_image_id
+  instance_type          = var.sentinel_instance_class
+  vpc_security_group_ids = [aws_security_group.mill_instance.id]
+  key_name               = var.ec2_keypair
+  user_data              = base64encode(templatefile("${path.module}/resources/cloud-init.tpl", merge(local.cloud_init_props, { node_type = "sentinel" })))
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      volume_size = 20
+      volume_type = "gp2"
+    }
   }
+
+  iam_instance_profile { name = data.aws_iam_instance_profile.duracloud.name }
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_autoscaling_group" "sentinel_asg" {
-  name                 = "${var.stack_name}-sentinel-asg"
-  launch_configuration = aws_launch_configuration.sentinel_launch_config.name
-  vpc_zone_identifier  = [data.aws_subnet.duracloud_a.id, data.aws_subnet.duracloud_c.id, data.aws_subnet.duracloud_d.id]
-  max_size             = 1
-  min_size             = 1
+  name                = "${var.stack_name}-sentinel-asg"
+  vpc_zone_identifier = [data.aws_subnet.duracloud_a.id, data.aws_subnet.duracloud_c.id, data.aws_subnet.duracloud_d.id]
+  max_size            = 1
+  min_size            = 1
+
+  launch_template {
+    id      = aws_launch_template.sentinel_launch_template.id
+    version = aws_launch_template.sentinel_launch_template.latest_version
+  }
+
   tag {
     key                 = "Name"
     value               = "${var.stack_name}-sentinel"

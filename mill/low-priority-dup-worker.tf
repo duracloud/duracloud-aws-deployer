97e04cdc4cf7fc34c22b63b913edabaab897e1ea
dbernstein <dabylon@gmail.com>
2024-12-13 11:14:35 -0800

    Replaces launch configuration resources with launch template (#54) (#29)
    Recommended by AWS. Required to use fuller range of instance types.
    
    Co-authored-by: Mark Cooper <mark.c.cooper@outlook.com>

--- mill/low-priority-dup-worker.tf
+++ mill/low-priority-dup-worker.tf
@@ -1,29 +1,46 @@
 # config the low priority dup worker launch config, autoscaling group, alarms, etc
 
-resource "aws_launch_configuration" "low_priority_dup_worker_launch_config" {
-  name_prefix          = "${var.stack_name}-low_priority_dup-worker-launch-config_"
-  image_id             = local.node_image_id
-  instance_type        = var.worker_instance_class
-  iam_instance_profile = data.aws_iam_instance_profile.duracloud.name
-  security_groups      = [aws_security_group.mill_instance.id]
-  key_name             = var.ec2_keypair
-  spot_price           = var.worker_spot_price
-  user_data            = templatefile("${path.module}/resources/cloud-init.tpl", merge(local.cloud_init_props, { node_type = "dup-worker" }))
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 60
+resource "aws_launch_template" "low_priority_dup_worker_launch_template" {
+  name_prefix            = "${var.stack_name}-low_priority_dup-worker-launch-template_"
+  image_id               = local.node_image_id
+  instance_type          = var.worker_instance_class
+  vpc_security_group_ids = [aws_security_group.mill_instance.id]
+  key_name               = var.ec2_keypair
+  user_data              = base64encode(templatefile("${path.module}/resources/cloud-init.tpl", merge(local.cloud_init_props, { node_type = "dup-worker" })))
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+    ebs {
+      volume_size = 60
+      volume_type = "gp2"
+    }
   }
+
+  iam_instance_profile { name = data.aws_iam_instance_profile.duracloud.name }
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      max_price = var.worker_spot_price
+    }
+  }
+
   lifecycle {
     create_before_destroy = true
   }
 }
 
 resource "aws_autoscaling_group" "low_priority_dup_worker_asg" {
-  name                 = "${var.stack_name}-low_priority_dup-worker-asg"
-  launch_configuration = aws_launch_configuration.low_priority_dup_worker_launch_config.name
-  vpc_zone_identifier  = [data.aws_subnet.duracloud_a.id, data.aws_subnet.duracloud_c.id, data.aws_subnet.duracloud_d.id]
-  max_size             = var.low_priority_dup_worker_max
-  min_size             = 0
+  name                = "${var.stack_name}-low_priority_dup-worker-asg"
+  vpc_zone_identifier = [data.aws_subnet.duracloud_a.id, data.aws_subnet.duracloud_c.id, data.aws_subnet.duracloud_d.id]
+  max_size            = var.low_priority_dup_worker_max
+  min_size            = 0
+
+  launch_template {
+    id      = aws_launch_template.low_priority_dup_worker_launch_template.id
+    version = aws_launch_template.low_priority_dup_worker_launch_template.latest_version
+  }
+
   tag {
     key                 = "Name"
     value               = "${var.stack_name}-low-priority-duplicate-worker"
